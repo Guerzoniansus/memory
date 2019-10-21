e57cd9e09124fb26a2a3eb53346f0f4da2a33ecf
ehhfrank <frank2001@hotmail.nl>
2019-10-21 12:13:38 +0200

    Add files via upload

---
+++ Voorkant/Voorkant/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Voorkant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            // quit knop die ervoor zorgt dat de app afsluit
+            Application.Current.Shutdown();
+        }
+
+        private void Highscore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            // hier moet code komen dat die naar window1 gaat
+            NewGame NewGame = new NewGame();
+            
+            NewGame.Show();
+
+            this.Close();
+
+        }
+
+
+    }
+}
