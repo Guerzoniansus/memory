514019f1028ac5f99c2297e09966911ce472ccc8
jessegerrit@gmail.com <Jessegerrit@gmail.com>
2019-10-28 18:07:33 +0100

    Some changes

--- Memory Game/Memory Game/DifficultyWindow.xaml.cs
+++ Memory Game/Memory Game/DifficultyWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace difficulty
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DifficultyWindow : Window
     {
-        public MainWindow()
+        public DifficultyWindow()
         {
 
         }
