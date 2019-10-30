609acb29280a95ed611ea9d162a0cee95b64a067
ChillWire <thijs.akkerman@student.nhlstenden.com>
2019-10-30 15:33:47 +0100

    Extra dingen en comments

--- Memory Game/Memory Game/NewGame.xaml.cs
+++ Memory Game/Memory Game/NewGame.xaml.cs
@@ -24,47 +24,41 @@ namespace Memory_Game
         public NewGame()
         {
             InitializeComponent();
-         
-        }
- //       if (Difficulty == Hard)
-	//{
+            if (Game.GetGame().GetDifficulty(Hard))
+            {
+                CardAmount.
+            }
 
-	//}
-       
-       
+        }
+         
+        
         private void SpBtn_Click(Object sender, RoutedEventArgs e)
         {
-           
+           // Set game op Singleplayer
+
             P2Input.Visibility = Visibility.Hidden;
             P2Text.Visibility = Visibility.Hidden;
             Game.GetGame().SetMultiplayer(false);
         }
         private void MpBtn_Click(Object sender, RoutedEventArgs e)
         {
+            // Set game op Multiplayer
+
             P2Input.Visibility = Visibility.Visible;
             P2Text.Visibility = Visibility.Visible;
             Game.GetGame().SetMultiplayer(true);
             
         }
 
-        private void P1Input_KeyDown_1(object sender, KeyEventArgs e)
-        {
-            if (e.Key >= Key.A && e.Key <= Key.Z)
-            {
-                e.Handled = false;
-            }
-            else
-            {
-                e.Handled = true;
-            }
-        }
+    
 
         private void ProceedBtn_Click(Object sender, RoutedEventArgs e)
         {
-
+            // Set player Names
             string player1 = P1Input.Text;
             string player2 = P2Input.Text;
-
+            
+            // Alleen alphanumerieke input voor player1
             char[] letters = player1.ToCharArray();
             
 
@@ -73,23 +67,53 @@ namespace Memory_Game
 
                 if (!(char.IsLetter(letter)) && (!(char.IsNumber(letter)))){
 
-                    MessageBox.Show("Alleen letters en cijfers, geen speciale tekens!");                    
+                    MessageBox.Show("Alleen letters en cijfers, geen speciale tekens!");
+                    
+                    
+                }                                        
+                
+            }
+            // Alleen alphanumerieke input voor player2
+            char[] letters2 = player2.ToCharArray();
+
+
+            foreach (char letter in letters2)
+            {
+
+                if (!(char.IsLetter(letter)) && (!(char.IsNumber(letter))))
+                {
+
+                    MessageBox.Show("Alleen letters en cijfers, geen speciale tekens!");
+
+
                 }
 
             }
+           
+            // Laat speler 2 naam leeg
+            if (player2 == null)
+            {
+                player2 = "";
+            }
 
+            // Set playernames vast
             Game.GetGame().SetPlayers(player1, player2);
 
+            // Set de geselecteerde hoeveelheid kaarten
             string CardAmountString = CardAmount.Text;
-            Game.GetGame().SetAmountOfCards(CardAmountString);
+            Game.GetGame().SetAmountOfCards(Convert.ToInt32(CardAmountString));
 
+            // Opent het speelveld
             GameWindow gameWindow = new GameWindow();
             gameWindow.Show();
             this.Close();
+
         }
 
         private void BackBtn_Click(Object sender, RoutedEventArgs e)
         {
+            // Returned naar het Main Menu
+
             MainWindow mainWindow = new MainWindow();
 
             mainWindow.Show();
